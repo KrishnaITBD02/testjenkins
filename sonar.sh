dadb8db52ad899db31847dece3bbbf19564c5f4a
Krishna Sharma <124158389+KrishnaITBD02@users.noreply.github.com>
2023-04-10 13:04:01 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -3,3 +3,5 @@ sonar-scanner \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://127.0.0.1:9000 \
   -Dsonar.login=sqp_6bd5f2ae23f643bb9de396a90b7fe26c294a92c5
+  -Dsonar.language=python
+  -Dsonar.python.version=3.8
